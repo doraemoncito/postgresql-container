f2125a4fa50d65c7d895f0c591deb50c7a534c47
Jose Hernandez <jh3875@gmail.com>
2020-04-19 17:03:50 +0100

    Made Dockerfile compatible with previous versions of the base image. e.g. postgres:1X-alpine

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -16,7 +16,7 @@ echo "============================================================"
 # https://www.postgresql.org/docs/11/client-authentication.html
 pg_ctl --options "-c listen_addresses='localhost'" --wait restart
 
-cd db
+cd db || exit
 /flyway/flyway -user="$POSTGRES_USER" -password="$POSTGRES_PASSWORD" -configFiles=/db/configuration/postgres/flyway.properties -url="jdbc:postgresql://localhost:$PGPORT/$POSTGRES_DB?user=$POSTGRES_USER" -locations="filesystem:/db/migration" info migrate info
 cd..
 
